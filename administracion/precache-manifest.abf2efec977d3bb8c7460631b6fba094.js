22b794e88608723a2f5ec73200f739998c08a1f8
paulantezana <contacto@paulantezana.com>
2019-04-11 15:15:55 -0500

    new updates

---
+++ administracion/precache-manifest.abf2efec977d3bb8c7460631b6fba094.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = [
+  {
+    "revision": "df5c683746b83c1c2dd2",
+    "url": "/p__Teacher.async.js"
+  },
+  {
+    "revision": "091082e4edde8d2a72db",
+    "url": "/vendors.chunk.css"
+  },
+  {
+    "revision": "faddacf6d3c6ffca8a2b",
+    "url": "/p__User__Forgot.chunk.css"
+  },
+  {
+    "revision": "b19327e3e83ce06113ae14c0d0497903",
+    "url": "/pwacompat.min.js"
+  },
+  {
+    "revision": "6fa2c0af13bbab518f2f",
+    "url": "/umi.js"
+  },
+  {
+    "revision": "6fa2c0af13bbab518f2f",
+    "url": "/umi.css"
+  },
+  {
+    "revision": "091082e4edde8d2a72db",
+    "url": "/vendors.async.js"
+  },
+  {
+    "revision": "54096b333517fc01da66",
+    "url": "/p__User__Forgot__models__forgot.js.async.js"
+  },
+  {
+    "revision": "899c7dc15fbfac9f105a",
+    "url": "/p__Company.chunk.css"
+  },
+  {
+    "revision": "3f445574a19643032699",
+    "url": "/p__Exception__404.async.js"
+  },
+  {
+    "revision": "205c46a6e21a6fe07bfd",
+    "url": "/p__Exception__500.async.js"
+  },
+  {
+    "revision": "e179bee4cf31a249be46",
+    "url": "/p__Program.chunk.css"
+  },
+  {
+    "revision": "e179bee4cf31a249be46",
+    "url": "/p__Program.async.js"
+  },
+  {
+    "revision": "3962ced77310e0f5fa94",
+    "url": "/p__Program__Setting.async.js"
+  },
+  {
+    "revision": "0f7a6cf925bacf01d319",
+    "url": "/p__Student.chunk.css"
+  },
+  {
+    "revision": "0f7a6cf925bacf01d319",
+    "url": "/p__Student.async.js"
+  },
+  {
+    "revision": "b65fb9ae791ad55fe4d3",
+    "url": "/p__Student__Detail.chunk.css"
+  },
+  {
+    "revision": "b65fb9ae791ad55fe4d3",
+    "url": "/p__Student__Detail.async.js"
+  },
+  {
+    "revision": "d6f0e8c486bc22130e4c",
+    "url": "/p__Subsidiary.chunk.css"
+  },
+  {
+    "revision": "d6f0e8c486bc22130e4c",
+    "url": "/p__Subsidiary.async.js"
+  },
+  {
+    "revision": "df5c683746b83c1c2dd2",
+    "url": "/p__Teacher.chunk.css"
+  },
+  {
+    "revision": "0ff560ac9b53d397a96b",
+    "url": "/p__User__Term.async.js"
+  },
+  {
+    "revision": "817facb2615887b436f6",
+    "url": "/p__User__PostLogin__models__postlogin.js.async.js"
+  },
+  {
+    "revision": "faddacf6d3c6ffca8a2b",
+    "url": "/p__User__Forgot.async.js"
+  },
+  {
+    "revision": "2faceac2a1f556494297",
+    "url": "/p__User__PostLogin.async.js"
+  },
+  {
+    "revision": "3b48e2ba9dbdae489f82",
+    "url": "/p__User__Login.chunk.css"
+  },
+  {
+    "revision": "3b48e2ba9dbdae489f82",
+    "url": "/p__User__Login.async.js"
+  },
+  {
+    "revision": "2faceac2a1f556494297",
+    "url": "/p__User__PostLogin.chunk.css"
+  },
+  {
+    "revision": "9747798575de9455f10882bd55c5ca58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "899c7dc15fbfac9f105a",
+    "url": "/p__Company.async.js"
+  },
+  {
+    "revision": "8d5f95ee5051e1b31e5d2b19a782124b",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "b1df02ad604cb144a100",
+    "url": "/p__404.async.js"
+  },
+  {
+    "revision": "810fe3bd6dcdce3c73c74005c16dfcbc",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "379b941663f59713ebfe",
+    "url": "/layouts__UserLayout.chunk.css"
+  },
+  {
+    "revision": "379b941663f59713ebfe",
+    "url": "/layouts__UserLayout.async.js"
+  },
+  {
+    "revision": "27442d3c519e5ff0c34f",
+    "url": "/layouts__BasicLayout.chunk.css"
+  },
+  {
+    "revision": "27442d3c519e5ff0c34f",
+    "url": "/layouts__BasicLayout.async.js"
+  },
+  {
+    "revision": "d1475c5d9ce5b07b6adc03ce2e63c8f8",
+    "url": "/icons/icon-512x512.png"
+  },
+  {
+    "revision": "82276a0586e92be33185bd3f781fd512",
+    "url": "/icons/icon-192x192.png"
+  },
+  {
+    "revision": "593a9bfa5925caffbb5137e8b15cc40d",
+    "url": "/icons/icon-128x128.png"
+  },
+  {
+    "revision": "8c2c13a518d8f22c4e99",
+    "url": "/20.chunk.css"
+  },
+  {
+    "revision": "8c2c13a518d8f22c4e99",
+    "url": "/20.async.js"
+  }
+];
---
+++ alumno/precache-manifest.09f01413ef00eabf5db5b33af07f7858.js
@@ -0,0 +1,222 @@
+self.__precacheManifest = [
+  {
+    "revision": "1862e5eb72f8d4ac42a5",
+    "url": "/p__Monitoring__Quiz__Question.chunk.css"
+  },
+  {
+    "revision": "5210166093f9850b4a52",
+    "url": "/vendors.chunk.css"
+  },
+  {
+    "revision": "1862e5eb72f8d4ac42a5",
+    "url": "/p__Monitoring__Quiz__Question.async.js"
+  },
+  {
+    "revision": "b19327e3e83ce06113ae14c0d0497903",
+    "url": "/pwacompat.min.js"
+  },
+  {
+    "revision": "ab56ee53a9e2564d9e16",
+    "url": "/umi.js"
+  },
+  {
+    "revision": "ab56ee53a9e2564d9e16",
+    "url": "/umi.css"
+  },
+  {
+    "revision": "5210166093f9850b4a52",
+    "url": "/vendors.async.js"
+  },
+  {
+    "revision": "7bee697c9e718708581b",
+    "url": "/p__Student__History.async.js"
+  },
+  {
+    "revision": "d2ba496217c983332766",
+    "url": "/p__Book.chunk.css"
+  },
+  {
+    "revision": "cb4a17d31c87b167c17b",
+    "url": "/p__Book__Single.chunk.css"
+  },
+  {
+    "revision": "cb4a17d31c87b167c17b",
+    "url": "/p__Book__Single.async.js"
+  },
+  {
+    "revision": "ade1eb8aabf5b19223ee",
+    "url": "/p__Dashboard.chunk.css"
+  },
+  {
+    "revision": "ade1eb8aabf5b19223ee",
+    "url": "/p__Dashboard.async.js"
+  },
+  {
+    "revision": "7f3030077a034501d50a",
+    "url": "/p__Exception__403.async.js"
+  },
+  {
+    "revision": "ab1848f9f74776c90bef",
+    "url": "/p__Exception__404.async.js"
+  },
+  {
+    "revision": "2684b8410f28edfbc9fd",
+    "url": "/p__Exception__500.async.js"
+  },
+  {
+    "revision": "ec4024a50da0fc039185",
+    "url": "/p__Messenger.chunk.css"
+  },
+  {
+    "revision": "ec4024a50da0fc039185",
+    "url": "/p__Messenger.async.js"
+  },
+  {
+    "revision": "c4da823b70de84559218",
+    "url": "/p__Monitoring__Diplomat.chunk.css"
+  },
+  {
+    "revision": "c4da823b70de84559218",
+    "url": "/p__Monitoring__Diplomat.async.js"
+  },
+  {
+    "revision": "e95b94617a53ede42236",
+    "url": "/p__Monitoring__Diplomat__Quiz.chunk.css"
+  },
+  {
+    "revision": "e95b94617a53ede42236",
+    "url": "/p__Monitoring__Diplomat__Quiz.async.js"
+  },
+  {
+    "revision": "e088fa5fa9152600f42c",
+    "url": "/p__Monitoring__Poll.chunk.css"
+  },
+  {
+    "revision": "e088fa5fa9152600f42c",
+    "url": "/p__Monitoring__Poll.async.js"
+  },
+  {
+    "revision": "bf18fbaab0608d0f0398",
+    "url": "/p__Monitoring__Poll__Question.async.js"
+  },
+  {
+    "revision": "ea0c264c611275afdd2e",
+    "url": "/p__Monitoring__Quiz.chunk.css"
+  },
+  {
+    "revision": "ea0c264c611275afdd2e",
+    "url": "/p__Monitoring__Quiz.async.js"
+  },
+  {
+    "revision": "45cff578188e82d52a56",
+    "url": "/p__User__Profile.async.js"
+  },
+  {
+    "revision": "1e03ed81ea15ea62d591",
+    "url": "/p__User__PostLogin__models__postlogin.js.async.js"
+  },
+  {
+    "revision": "64c7f30e4303162effb8",
+    "url": "/p__Student.async.js"
+  },
+  {
+    "revision": "7bee697c9e718708581b",
+    "url": "/p__Student__History.chunk.css"
+  },
+  {
+    "revision": "c472ad1cc6b605be49ad",
+    "url": "/p__User__PostLogin.async.js"
+  },
+  {
+    "revision": "4b144f9627a5251bf863",
+    "url": "/p__Student__Practice.async.js"
+  },
+  {
+    "revision": "a2a2f66c05a92d5526b9",
+    "url": "/p__Student__Setting.chunk.css"
+  },
+  {
+    "revision": "a2a2f66c05a92d5526b9",
+    "url": "/p__Student__Setting.async.js"
+  },
+  {
+    "revision": "d45c94cbece13d77f36c",
+    "url": "/p__User__Forgot.chunk.css"
+  },
+  {
+    "revision": "d45c94cbece13d77f36c",
+    "url": "/p__User__Forgot.async.js"
+  },
+  {
+    "revision": "3dd080a853723bd4aca1",
+    "url": "/p__User__Forgot__models__forgot.js.async.js"
+  },
+  {
+    "revision": "34b0876cd2a0ac306629",
+    "url": "/p__User__Login.chunk.css"
+  },
+  {
+    "revision": "34b0876cd2a0ac306629",
+    "url": "/p__User__Login.async.js"
+  },
+  {
+    "revision": "c472ad1cc6b605be49ad",
+    "url": "/p__User__PostLogin.chunk.css"
+  },
+  {
+    "revision": "0dd5729a5829e20df69effb5465aeaa4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2ba496217c983332766",
+    "url": "/p__Book.async.js"
+  },
+  {
+    "revision": "3ccec467e572aa81840708c5e79cb610",
+    "url": "/icons/icon-192x192.png"
+  },
+  {
+    "revision": "b1df02ad604cb144a100",
+    "url": "/p__404.async.js"
+  },
+  {
+    "revision": "b0e524c9550cbcc0424cd6e5141db520",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "436bc0e5ca35742db163",
+    "url": "/layouts__UserLayout.chunk.css"
+  },
+  {
+    "revision": "436bc0e5ca35742db163",
+    "url": "/layouts__UserLayout.async.js"
+  },
+  {
+    "revision": "b74da9dce38f53db3d7b",
+    "url": "/layouts__BasicLayout.chunk.css"
+  },
+  {
+    "revision": "b74da9dce38f53db3d7b",
+    "url": "/layouts__BasicLayout.async.js"
+  },
+  {
+    "revision": "6c6c77e3c0ac4b9f55e3dc58a12eda32",
+    "url": "/icons/icon-512x512.png"
+  },
+  {
+    "revision": "3999714cf2bbe32514cf70547b830389",
+    "url": "/icons/icon-128x128.png"
+  },
+  {
+    "revision": "d7abaf2bce9a8c41016cfaef6a4da8eb",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "f5a2f33a7f9153594697",
+    "url": "/28.chunk.css"
+  },
+  {
+    "revision": "f5a2f33a7f9153594697",
+    "url": "/28.async.js"
+  }
+];
